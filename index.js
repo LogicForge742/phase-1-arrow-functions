c595e53300db5d4ceafe61f2a1376a4b2998ee3e
Milton Ngeno <ngenomilton10@gmail.com>
2025-06-16 15:25:42 +0300

    adds Arrow Functions

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+function foo() {
+  return 'bar';
+}
+console.log(foo())
+*/
+/*
+const foo = function() {
+  return 'bar';
+}
+console.log(foo())
+*/
+/*
+const nums = [1,2,3];
+const squares = nums.map(x => x ** 2); 
+squares; //=> [1,4,9]
+nums; //=> [1,2,3]
+*/
+/*
+finishedItems = overdueTodoItems.map( item => item.className = "complete" );
+header.innerText = `You finished ${finishedItems.length} items!`;   */
+/*
+let divide=function(){
+    let a= 2000
+    let b = 100
+   return  a/b
+}
+*/
+const divide = () => 2000 / 100;
+const square = x=> x*x
+const add = (p,r)=> p+r
